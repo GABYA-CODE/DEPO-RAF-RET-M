711ce34e0e8c9492c028b6c35f458f69c5f65fef
ibrahim kanber <ibrahim.kanber@qconnex.com>
2025-12-25 08:26:51 +0100

    feat: update Firebase configuration to use environment variables and upgrade Node.js version to 20

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRysIhPcqL2rXwHUvScogOWbCd_k-wmwI",
-  authDomain: "depo-paketleme.firebaseapp.com",
-  projectId: "depo-paketleme",
-  storageBucket: "depo-paketleme.firebasestorage.app",
-  messagingSenderId: "242943188992",
-  appId: "1:242943188992:web:38f62ac67d5ea544e8f8e5",
-  measurementId: "G-79Y74VNJ43",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
